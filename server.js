f699869505ff5c3d3a10ed5d325d255ba66bc24a
Abdulrahman-Mashaal <abdul.attiamashaal@gmail.com>
2021-08-09 08:41:04 +0200

    ssetup server file

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express =  require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 7000;
 
 // Start up an instance of app
 
@@ -11,9 +16,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+app.post('/gatheringData', (req, res) =>{
+    projectData = { ...req.body};
+    res.send();
+})
+
+app.get('/retrieveData', (req, res) =>{
+    res.send(projectData);
+})
+
+const server = app.listen(port, listening);
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost:${port}`);
+};
